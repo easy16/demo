0e22c8aaf569e8d6f3a43fc3143b2496c35695a2
liujunqiang <liujunqiang@corp.netease.com>
2019-03-13 17:32:10 +0800

    add spring demo

---
+++ spring/HelloSpring/src/com/tutorialspoint/ByeWorld.java
@@ -0,0 +1,10 @@
+package com.tutorialspoint;
+public class ByeWorld {
+   private String message;
+   public void setMessage(String message){
+      this.message  = message;
+   }
+   public void getMessage(){
+      System.out.println("Your Message : " + message);
+   }
+}
---
+++ spring/HelloSpring/src/com/tutorialspoint/MainApp.java
@@ -0,0 +1,15 @@
+package com.tutorialspoint;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class MainApp {
+   public static void main(String[] args) {
+      ApplicationContext context = 
+             new ClassPathXmlApplicationContext("Beans.xml");
+      HelloWorld obj = (HelloWorld) context.getBean("helloWorld");
+      obj.getMessage();
+      
+      ByeWorld obj2 = (ByeWorld) context.getBean("byeWorld");
+      obj2.getMessage();
+   }
+}
